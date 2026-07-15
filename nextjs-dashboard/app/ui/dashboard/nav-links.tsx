bbb5ca2aaae17cc6830bdabde63313090a158789
Idealyy <idealytsitohaina@gmail.com>
2026-07-15 14:41:04 +0300

    feat> translation

--- nextjs-dashboard/app/ui/dashboard/nav-links.tsx
+++ nextjs-dashboard/app/ui/dashboard/nav-links.tsx
@@ -1,36 +1,47 @@
-'use client'
+"use client";
+
 import {
   UserGroupIcon,
   HomeIcon,
   DocumentDuplicateIcon,
-} from '@heroicons/react/24/outline';
+} from "@heroicons/react/24/outline";
 
-import {usePathname} from 'next/navigation';
-import clsx from 'clsx';
-import Link from 'next/link';
-// Map of links to display in the side navigation.
-// Depending on the size of the application, this would be stored in a database.
+import { usePathname } from "next/navigation";
+import { useTranslations } from "next-intl";
+import clsx from "clsx";
+import Link from "next/link";
+
 const links = [
-  { name: 'Home', href: '/dashboard', icon: HomeIcon },
+  { key: "home", href: "/dashboard", icon: HomeIcon },
   {
-    name: 'Invoices',
-    href: '/dashboard/invoices',
+    key: "invoices",
+    href: "/dashboard/invoices",
     icon: DocumentDuplicateIcon,
   },
-  { name: 'Customers', href: '/dashboard/customers', icon: UserGroupIcon },
-  { name: 'Upload', href: '/dashboard/upload', icon: UserGroupIcon },
-
+  {
+    key: "customers",
+    href: "/dashboard/customers",
+    icon: UserGroupIcon,
+  },
+  {
+    key: "upload",
+    href: "/dashboard/upload",
+    icon: UserGroupIcon,
+  },
 ];
 
 export default function NavLinks() {
   const pathname = usePathname();
+  const t = useTranslations("Navigation");
+
   return (
     <>
       {links.map((link) => {
         const LinkIcon = link.icon;
+
         return (
           <Link
-            key={link.name}
+            key={link.key}
             href={link.href}
             className={clsx(
               "flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3",
@@ -40,7 +51,7 @@ export default function NavLinks() {
             )}
           >
             <LinkIcon className="w-6" />
-            <p className="hidden md:block">{link.name}</p>
+            <p className="hidden md:block">{t(link.key)}</p>
           </Link>
         );
       })}
